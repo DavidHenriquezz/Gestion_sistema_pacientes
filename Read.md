bb974282322956abc42cd773e716f2b2833d79f2
DavidHenriquezz <163459096+DavidHenriquezz@users.noreply.github.com>
2024-04-19 23:57:57 +0000

    Initial commit

---
+++ Read.md
@@ -0,0 +1,143 @@
+# Sistema de Gestión de Pacientes en Hospital
+
+## Descripción
+
+Este sistema permite a los usuarios gestionar la atención de pacientes en un hospital. Los usuarios pueden ingresar nuevos pacientes, asignar su prioridad, mostrar la lista de espera, ver quien es el siguiente en atender, etc.. Esto esta diseñado para mejorar la eficiencia de atencion poniendo como prioridad las personas que tengan situaciones más criticas.
+
+## Cómo compilar y ejecutar
+
+Dado que el sistema está diseñado para ser accesible y fácil de probar, recomendamos usar Repl.it para una rápida configuración y ejecución. Sigue estos pasos para comenzar:
+
+1.- Visita Repl.it.
+
+2.- Crea una cuenta o inicia sesion.
+
+3.- Una vez en el inicio selecciona  "New Repl" y elige "Import from GitHub".
+
+4.- Pega este link 
+
+5.- Repl.it clonara el repositorio y te preparará un entorno de ejecución
+
+6.- Presiona el boton "Run" para ejecutar.
+
+## Funcionalidades
+
+### Funcionando correctamente:
+
+- Registrar pacientes con sus datos básicos y una prioridad inicial.
+- Asignar o modificar la prioridad de los pacientes.
+- Ver la lista de espera de pacientes, ordenada por prioridad y hora de registro.
+- Atender al siguiente paciente, respetando el orden de prioridad.
+- Ver la lista de pacientes según la prioridad seleccionada
+
+### A mejorar:
+
+- Modificar la interfaz para que sea más amigable
+- Permitir la modificación de datos del paciente
+
+## Ejemplo de uso
+
+**Paso 1: Registrar un Nuevo Paciente**
+
+Se comienza registrando un nuevo paciente que acaba de llegar al hospital.
+```
+========================================
+     Sistema de Gestión Hospitalaria
+========================================
+1) Registrar paciente
+2) Asignar prioridad a paciente
+3) Mostrar lista de espera
+4) Atender al siguiente paciente
+5) Mostrar pacientes por prioridad
+6) Salir
+Ingrese su opción: 1
+Registrar nuevo paciente
+-------------------------------
+Ingrese el nombre del paciente: Juan Leon
+Ingrese la edad del paciente: 20
+Ingrese el síntoma del paciente: Dolor de estomago
+Se registro a las 19:52:40
+-------------------------------
+```
+
+El sistema regista a "Juan Leon" con una prioridad "Baja" y guarda la hora en la que fue registrado. La prioridad puede ser cambiada posteriormente.
+
+**Paso 2: Asignar Prioridad a un Paciente**
+
+Tras la evaluación del doctor se decide que Juan León debe tener una prioridad alta.
+
+```
+========================================
+     Sistema de Gestión Hospitalaria
+========================================
+1) Registrar paciente
+2) Asignar prioridad a paciente
+3) Mostrar lista de espera
+4) Atender al siguiente paciente
+5) Mostrar pacientes por prioridad
+6) Salir
+Ingrese su opción: 2
+-------------------------------
+Datos del paciente:
+Nombre: Juan Leon
+Edad: 20
+Síntoma: Dolor de estomago
+Desea reasignar la prioridad?(s/n)
+-------------------------------
+```
+
+En esta situacion hay 2 posibles respuesta el "si"(s) o el "no"(n)
+
+si seleccionan "si" verán el siguiente
+
+```
+Seleccione que prioridad que le desea asignar:
+1) Alta
+2) Medio
+3) Baja
+
+```
+La prioridad se actualizará según el numero que ingrese (1,2,3), siendo estas las prioridades alta, media y baja.
+
+si seleccionan "no" simplemente seguirá al siguiente paciente
+
+para finalmente mostrar un mensaje cuando se termine de recorrer la lista de pacientes
+
+**Paso 3: Ver la Lista de Espera**
+
+El usuario revisa la lista de espera para ver todos los pacientes y sus prioridades.
+
+```
+========================================
+     Sistema de Gestión Hospitalaria
+========================================
+1) Registrar paciente
+2) Asignar prioridad a paciente
+3) Mostrar lista de espera
+4) Atender al siguiente paciente
+5) Mostrar pacientes por prioridad
+6) Salir
+Ingrese su opción: 3
+-------------------------------
+Nombre: Juan Leon - Prioridad: Alta
+Fue ingresado a las 19:52:40
+-------------------------------
+```
+
+La lista muestra a Juan Leon en la parte superior, indicando su prioridad alta y que es la siguiente en línea para recibir atención.
+
+**Paso 4: Atender al Siguiente Paciente**
+
+Juan Leon es el siguiente para ser atendido basándose en su prioridad.
+
+```
+-------------------------------
+El siguiente paciente es: Juan Leon
+Edad: 20
+Sintoma: Dolor de estomago
+Prioridad: Alta
+Fue ingresado a las 19:52:40
+-------------------------------
+```
+
+El sistema muestra que Juan Leon está siendo atendido y lo elimina de la lista de espera.
---
+++ main.c
@@ -0,0 +1,295 @@
+#include "tdas/list.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+typedef struct paciente {
+  char nombre[101];
+  int edad;
+  char sintoma[101];
+  char prioridad[101];
+  char hora[9];
+} paciente;
+
+// Función para limpiar la pantalla
+void limpiarPantalla() { system("clear"); }
+
+void presioneTeclaParaContinuar() {
+  puts("Presione una tecla para continuar...");
+  getchar(); // Consume el '\n' del buffer de entrada
+  getchar(); // Espera a que el usuario presione una tecla
+}
+
+// Menú principal
+void mostrarMenuPrincipal() {
+  limpiarPantalla();
+  puts("========================================");
+  puts("     Sistema de Gestión Hospitalaria");
+  puts("========================================");
+
+  puts("1) Registrar paciente");
+  puts("2) Asignar prioridad a paciente");
+  puts("3) Mostrar lista de espera");
+  puts("4) Atender al siguiente paciente");
+  puts("5) Mostrar pacientes por prioridad");
+  puts("6) Salir");
+}
+
+void registrar_paciente(List *pacientes) {
+  paciente *paciente_actual = (paciente *)malloc(sizeof(paciente));
+  if (paciente_actual == NULL)
+    exit(EXIT_FAILURE);
+
+  struct tm *fecha_hora;
+  time_t segundos = time(NULL);
+
+  fecha_hora = localtime(&segundos);
+
+  int hora = fecha_hora->tm_hour - 4;
+  int minutos = fecha_hora->tm_min;
+  int segundos_actual = fecha_hora->tm_sec;
+
+  char hora_str[9];
+
+  sprintf(hora_str, "%02d:%02d:%02d", hora, minutos, segundos_actual);
+  strcpy(paciente_actual->hora, hora_str);
+
+  printf("Registrar nuevo paciente\n");
+  printf("-------------------------------\n");
+  printf("Ingrese el nombre del paciente: ");
+  scanf(" %[^\n]s", paciente_actual->nombre);
+  printf("Ingrese la edad del paciente: ");
+  scanf("%i", &paciente_actual->edad);
+  printf("Ingrese el síntoma del paciente: ");
+  scanf(" %[^\n]s", paciente_actual->sintoma);
+  strcpy(paciente_actual->prioridad, "Baja");
+  printf("Se registro a las %s\n", paciente_actual->hora);
+  printf("-------------------------------\n");
+  list_pushBack(pacientes, paciente_actual);
+}
+
+void reasignar(paciente *paciente) {
+  char opcion, priori;
+  printf("-------------------------------\n");
+  printf("Datos del paciente:\n");
+  printf("Nombre: %s\n", paciente->nombre);
+  printf("Edad: %i\n", paciente->edad);
+  printf("Síntoma: %s\n", paciente->sintoma);
+  printf("Desea reasignar la prioridad?(s/n)\n");
+  printf("-------------------------------\n");
+  scanf(" %c", &opcion);
+
+  switch (opcion) {
+  case 's':
+    limpiarPantalla();
+    printf("Seleccione que prioridad que le desea asignar:\n");
+    printf("1) Alta\n");
+    printf("2) Medio\n");
+    printf("3) Baja\n");
+
+    scanf(" %c", &priori);
+    limpiarPantalla();
+    switch (priori) {
+    case '1':
+      strcpy(paciente->prioridad, "Alta");
+      break;
+    case '2':
+      strcpy(paciente->prioridad, "Medio");
+      break;
+    case '3':
+      strcpy(paciente->prioridad, "Baja");
+      break;
+    default:
+      puts("Opción no válida. Por favor, intente de nuevo.");
+    }
+    break;
+  case 'n':
+    limpiarPantalla();
+    break;
+  default:
+    puts("Opción no válida. Por favor, intente de nuevo.");
+    printf("-------------------------------\n");
+  }
+}
+
+void asignar_prioridad(List *pacientes, List *pacientesMedio,
+                       List *pacientesAlta, List *pacientesBaja) {
+  paciente *paciente = list_first(pacientes);
+  if (paciente == NULL) {
+    printf("-------------------------------\n");
+    printf("No hay pacientes registrados\n");
+    printf("-------------------------------\n");
+  } else {
+    while (paciente != NULL) {
+      reasignar(paciente);
+      if (strcmp(paciente->prioridad, "Alta") == 0) {
+        list_pushBack(pacientesAlta, paciente);
+      } else if (strcmp(paciente->prioridad, "Medio") == 0) {
+        list_pushBack(pacientesMedio, paciente);
+      } else {
+        list_pushBack(pacientesBaja, paciente);
+      }
+      paciente = list_next(pacientes);
+      list_popFront(pacientes);
+    }
+    printf("-------------------------------\n");
+    printf("Prioridades asignadas correctamente\n");
+    printf("-------------------------------\n");
+  }
+}
+void mostrar_lista_pacientes(List *pacientes, List *pacientesMedio, List *pacientesAlto) {
+  // Mostrar pacientes en la lista de espera
+  paciente *datoAlto = list_first(pacientesAlto);
+  paciente *datoMedio = list_first(pacientesMedio);
+  paciente *datoBaja = list_first(pacientes);
+  printf("-------------------------------\n");
+  if (datoAlto == NULL && datoMedio == NULL && datoBaja == NULL) {
+    printf("No hay pacientes en la cola de espera\n");
+    printf("-------------------------------\n");
+  } else {
+    while (datoAlto != NULL) {
+      printf("Nombre: %s - Prioridad: %s\n", datoAlto->nombre, datoAlto->prioridad);
+      printf("Fue ingresado a las %s\n", datoAlto->hora);
+      printf("-------------------------------\n");
+      datoAlto = list_next(pacientesAlto);
+    }
+    while (datoMedio != NULL) {
+      printf("Nombre: %s - Prioridad: %s\n", datoMedio->nombre, datoMedio->prioridad);
+      printf("Fue ingresado a las %s\n", datoMedio->hora);
+      printf("-------------------------------\n");
+      datoMedio = list_next(pacientesMedio);
+    }
+    while (datoBaja != NULL) {
+      printf("Nombre: %s - Prioridad: %s\n", datoBaja->nombre, datoBaja->prioridad);
+      printf("Fue ingresado a las %s\n", datoBaja->hora);
+      printf("-------------------------------\n");
+      datoBaja = list_next(pacientes);
+    }
+  }
+}
+void atender_paciente(List *pacientesBajo, List *pacientesMedio,
+                      List *pacienteAlto) {
+  limpiarPantalla();
+  printf("-------------------------------\n");
+  paciente *pacienteAlta = list_first(pacienteAlto);
+  if (pacienteAlta == NULL) {
+    paciente *pacienteMedio = list_first(pacientesMedio);
+    if (pacienteMedio == NULL) {
+      paciente *pacienteBajo = list_first(pacientesBajo);
+      if (pacienteBajo == NULL) {
+        printf("No hay pacientes en la cola de espera\n");
+      } else {
+        printf("El siguiente paciente es: %s\n", pacienteBajo->nombre);
+        printf("Edad: %i\n", pacienteBajo->edad);
+        printf("Sintoma: %s\n", pacienteBajo->sintoma);
+        printf("Prioridad: %s\n", pacienteBajo->prioridad);
+        printf("Fue ingresado a las %s\n", pacienteBajo->hora);
+        list_popFront(pacientesBajo);
+      }
+    } else {
+      printf("El siguiente paciente es: %s\n", pacienteMedio->nombre);
+      printf("Edad: %i\n", pacienteMedio->edad);
+      printf("Sintoma: %s\n", pacienteMedio->sintoma);
+      printf("Prioridad: %s\n", pacienteMedio->prioridad);
+      printf("Fue ingresado a las %s\n", pacienteMedio->hora);
+      list_popFront(pacientesMedio);
+    }
+  } else {
+    printf("El siguiente paciente es: %s\n", pacienteAlta->nombre);
+    printf("Edad: %i\n", pacienteAlta->edad);
+    printf("Sintoma: %s\n", pacienteAlta->sintoma);
+    printf("Prioridad: %s\n", pacienteAlta->prioridad);
+    printf("Fue ingresado a las %s\n", pacienteAlta->hora);
+    list_popFront(pacienteAlto);
+  }
+  printf("-------------------------------\n");
+}
+void mostrar_lista(List *lista) {
+  paciente *dato = list_first(lista);
+  printf("-------------------------------\n");
+  if (dato == NULL)
+    printf("No hay pacientes registrados\n");
+  else {
+    while (dato != NULL) {
+      printf("Nombre:");
+      printf("%s - Prioridad: %s\n", dato->nombre, dato->prioridad);
+      printf("Fue ingresado a las %s\n", dato->hora);
+      dato = list_next(lista);
+    }
+  }
+  printf("-------------------------------\n");
+}
+void mostrarPrioridad(List *pacientes, List *pacientesMedio,
+                      List *pacientesAlto) {
+  char eleccion;
+  printf("-------------------------------\n");
+  printf("Seleccione que prioridad desea ver:\n");
+  printf("1) Alta\n");
+  printf("2) Medio\n");
+  printf("3) Baja\n");
+  printf("-------------------------------\n");
+  scanf(" %c", &eleccion);
+  limpiarPantalla();
+  switch (eleccion) {
+  case '1':
+    mostrar_lista(pacientesAlto);
+    break;
+  case '2':
+    mostrar_lista(pacientesMedio);
+    break;
+  case '3':
+    mostrar_lista(pacientes);
+    break;
+  default:
+    printf("Opción inválida\n");
+    break;
+  }
+}
+
+int main() {
+  char opcion;
+  List *pacientes = list_create();
+  List *pacientesBaja = list_create();
+  List *pacientesMedio = list_create();
+  List *pacientesAlto = list_create();
+  // puedes usar una lista para gestionar los pacientes
+
+  do {
+    mostrarMenuPrincipal();
+    printf("Ingrese su opción: ");
+    scanf(" %c", &opcion); // Nota el espacio antes de %c para consumir el
+                           // newline anterior
+
+    switch (opcion) {
+    case '1':
+      registrar_paciente(pacientes);
+      break;
+    case '2':
+      asignar_prioridad(pacientes, pacientesMedio, pacientesAlto,
+                        pacientesBaja);
+      break;
+    case '3':
+      mostrar_lista_pacientes(pacientesBaja, pacientesMedio, pacientesAlto);
+      break;
+    case '4':
+      atender_paciente(pacientesBaja, pacientesMedio, pacientesAlto);
+      break;
+    case '5':
+      mostrarPrioridad(pacientesBaja, pacientesMedio, pacientesAlto);
+      break;
+    case '6':
+      puts("Saliendo del sistema de gestión hospitalaria...");
+      break;
+    default:
+      puts("Opción no válida. Por favor, intente de nuevo.");
+    }
+    presioneTeclaParaContinuar();
+
+  } while (opcion != '6');
+
+  // Liberar recursos, si es necesario
+  list_clean(pacientes);
+
+  return 0;
+}
